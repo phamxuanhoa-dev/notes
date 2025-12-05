9416f8b53bd1eb0551cafe7ba71fd2b4de78d6f8
phamxuanhoa-dev <2025107021@obmps.com>
2025-12-05 15:25:04 +0900

    5

--- .flaskenv
+++
@@ -1,3 +0,0 @@
-# File này giúp lệnh `flask` tự động tìm thấy ứng dụng của bạn.
-FLASK_APP=src.app:create_app
-FLASK_DEBUG=1
--- blocklist.py
+++
@@ -1,5 +0,0 @@
-"""
-This file contains the blocklist of JWT tokens. It is used for token revocation.
-For a production application, you should store this in a persistent database like Redis.
-"""
-BLOCKLIST = set()
